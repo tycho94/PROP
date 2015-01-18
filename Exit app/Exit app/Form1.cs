d606775fd4973ea602bbd85bf05a0daec379c82b
Tycho Hertogs <tycho94@hotmail.com>
2015-01-18 18:08:51 +0100

    Revert "initial commit"
    This reverts commit 624fb78884f118ef725088d2860deb2a82834115.

--- Exit app/Exit app/Form1.cs
+++ Exit app/Exit app/Form1.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -89,64 +88,3 @@ namespace Exit_app
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Phidgets.Events;
-using Phidgets;
-
-namespace Exit_app
-{
-    public partial class Form1 : Form
-    {
-        private RFID Reader;
-        //ExitClient current = new ExitClient("2800b39b49");
-        DatabaseConnection data = new DatabaseConnection();
-
-        public Form1()
-        {
-            InitializeComponent();
-            label3.Visible = false;
-
-            try
-            {
-                Reader = new RFID();
-                //Reader.Attach += new AttachEventHandler(ReaderAttached);
-                Reader.open();
-                Reader.waitForAttachment(3000);
-                Reader.Antenna = true;
-                Reader.LED = true;
-                Reader.Tag += new TagEventHandler(Leaving);
-                
-            }
-            catch (PhidgetException)
-            {
-                MessageBox.Show("RFID reader not found. \nExiting program.");
-                //Environment.Exit(1);
-            }
-        }
-        
-
-        public void Leaving(object sender, TagEventArgs e)
-        {
-            moneylbl.Text = "Money left on card: " + data.GetBalance(e.Tag).ToString();
-
-            if(data.GetRented(e.Tag) == true)
-            {
-                label3.Visible = true;
-            }
-            else
-            {
-                label3.Visible = false;
-            }
-        }
-    }
-}
->>>>>>> Stashed changes
